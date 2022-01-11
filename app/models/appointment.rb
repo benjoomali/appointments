5a1322d7821a6b680113778afefc0d46adbee057
Ben <benjoomali@gmail.com>
2022-01-11 15:58:31 -0800

    updating index dashboard page, day_of validation for appointments thru availability

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,24 +3,35 @@ class Appointment < ApplicationRecord
   belongs_to :form_entries, optional: true
 
   validates :start_time, :end_time, presence: true
-  validate :check_interval
+  validate :validate_interval
+  validate :validate_availability
   
-  #TODO:validate that availability exists
+  #TODO: 
   def check_availability
   end
 
   private
 
+  # validate that the appointment exists within an availability
+  def validate_availability
+    # find the date of the appointment trying to be made
+    day = self.start_time
+    # find all Availability records on the day the Appointment was created for
+    if Availability.where(start_time: day.all_day).blank?
+      errors.add(:appointment, "There are no availabilities on this day")
+    end
+  end
 
-  #validate that the appointment created is the exact interval set by calendar
-  def check_interval 
+  # validate that the appointment created is the exact interval set by calendar
+  def validate_interval 
     interval = self.calendar.interval 
     time = (end_time - start_time)/60
     if time.to_i != self.calendar.interval
       errors.add(:appointment, "This appointment does not meet the interval requirements")
     end
   end 
-  #validate that the time is correct
+
+  # validate that the time is correct
 
 
   #has_many :entry_appointments, :dependent => :destroy 
