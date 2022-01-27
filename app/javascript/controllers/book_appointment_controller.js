de176962e305a3cb0dc1d751d4d46b7519017d59
Ben <benjoomali@gmail.com>
2022-01-27 15:54:07 -0800

    stimulus book-appointment controller fills in form entry on slot click. yaay

--- app/javascript/controllers/book_appointment_controller.js
+++ app/javascript/controllers/book_appointment_controller.js
@@ -1,10 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = [ "slot", "selected" ]
+    static targets = [ "slot", "selected", "start", "end" ]
 
     fire(e) {
         console.log("boom")
         console.log(e.srcElement)
+        console.log(e.srcElement.dataset.startTime)
+
+        this.startTarget.value = e.srcElement.dataset.startTime
+        this.endTarget.value = e.srcElement.dataset.endTime
     }
 }
